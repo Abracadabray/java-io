759041692129e9208af75badddb2c036b8d9520e
Abracadabray <1260596810@qq.com>
2022-03-04 16:59:15 +0800

    java-io update

--- src/main/java/com/malongbao/io/netty/tcp_demo/NettyClient.java
+++ src/main/java/com/malongbao/io/netty/tcp_demo/NettyClient.java
@@ -1,6 +1,7 @@
 package com.malongbao.io.netty.tcp_demo;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
@@ -18,37 +19,20 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 @SuppressWarnings("all")
 public class NettyClient {
     public static void main(String[] args) throws Exception {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();//客户端需要一个事件循环组
+        Bootstrap bootstrap = new Bootstrap(); //创建客户端启动对象
+        bootstrap.group(workerGroup)//设置线程组
+                .handler(new MyChannelInitializer())//加入自己的处理器
+                .channel(NioSocketChannel.class);// 设置客户端通道的实现类(反射)
+        System.out.println("客户端 ok..");
+        ChannelFuture channelFuture = bootstrap.connect("127.0.0.1",9999).sync();  //关于 ChannelFuture 要分析，涉及到netty的异步模型
+        channelFuture.channel().closeFuture().sync(); //对关闭通道事件  进行监听
+    }
 
-        //客户端需要一个事件循环组
-        EventLoopGroup group = new NioEventLoopGroup();
-
-
-        try {
-            //创建客户端启动对象
-            //注意客户端使用的不是 ServerBootstrap 而是 Bootstrap
-            Bootstrap bootstrap = new Bootstrap();
-
-            //设置相关参数
-            bootstrap.group(group) //设置线程组
-                    .channel(NioSocketChannel.class) // 设置客户端通道的实现类(反射)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new NettyClientHandler()); //加入自己的处理器
-                        }
-                    });
-
-            System.out.println("客户端 ok..");
-
-            //启动客户端去连接服务器端
-            //关于 ChannelFuture 要分析，涉及到netty的异步模型
-            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6668).sync();
-            //对关闭通道事件  进行监听
-            channelFuture.channel().closeFuture().sync();
-        } finally {
-
-            group.shutdownGracefully();
-
-        }
+    public static class MyChannelInitializer extends ChannelInitializer {
+        @Override
+        protected void initChannel(Channel channel) throws Exception {
+            channel.pipeline().addLast(new NettyClientHandler()); //加入自己的处理器
+        };
     }
 }
--- src/main/java/com/malongbao/io/netty/tcp_demo/NettyClientHandler.java
+++ src/main/java/com/malongbao/io/netty/tcp_demo/NettyClientHandler.java
@@ -18,18 +18,17 @@ public class NettyClientHandler extends ChannelInboundHandlerAdapter {
 
     //当通道就绪就会触发该方法
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("client " + ctx);
-        ctx.writeAndFlush(Unpooled.copiedBuffer("hello, server: (>^ω^<)喵", CharsetUtil.UTF_8));
+    public void channelActive(ChannelHandlerContext channelHandlerContext) throws Exception {
+        System.out.println("client " + channelHandlerContext);
+        channelHandlerContext.writeAndFlush(Unpooled.copiedBuffer("hello, server: (>^ω^<)喵", CharsetUtil.UTF_8));
     }
 
     //当通道有读取事件时，会触发
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-
+    public void channelRead(ChannelHandlerContext channelHandlerContext, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
         System.out.println("服务器回复的消息:" + buf.toString(CharsetUtil.UTF_8));
-        System.out.println("服务器的地址： " + ctx.channel().remoteAddress());
+        System.out.println("服务器的地址： " + channelHandlerContext.channel().remoteAddress());
     }
 
     @Override
